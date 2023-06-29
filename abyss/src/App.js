5e0cc0d7b0dee09d208d2f01813b73c663e9a3e4
Mo Azza <ssbsdmg@gmail.com>
2023-06-29 22:36:00 +0100

    Fixed path for game display
    - fixed a bug where the game wouldn't display due to incorrect path

--- abyss/src/App.js
+++ abyss/src/App.js
@@ -1,48 +1,42 @@
-import React, { useEffect } from "react";
-import { Unity, useUnityContext } from "react-unity-webgl";
-import HomePage from "./Components/HomePage";
-import styles from "./Components/ComponentCss/HomePage.module.css";
-import MainButton from "./Components/UI/MainButton";
-import { ConnectButton } from "@suiet/wallet-kit";
-import "./overrideSuiButton.css";
+import React, { useEffect } from 'react';
+import { Unity, useUnityContext } from 'react-unity-webgl';
+import HomePage from './Components/HomePage';
+import styles from './Components/ComponentCss/HomePage.module.css';
+import MainButton from './Components/UI/MainButton';
+import { ConnectButton } from '@suiet/wallet-kit';
+import './overrideSuiButton.css';
 
 function App() {
   const { unityProvider, requestFullscreen } = useUnityContext({
-    loaderUrl: "Build/Gamev2.loader",
-    dataUrl: "Build/Gamev2.data",
-    frameworkUrl: "Build/Gamev2.framework",
-    codeUrl: "Build/Gamev2.wasm",
+    loaderUrl: '/Build/Gamev1.loader.js',
+    dataUrl: '/Build/Gamev1.data',
+    frameworkUrl: '/Build/Gamev1.framework.js',
+    codeUrl: '/Build/Gamev1.wasm',
   });
 
   const requestFullScr = () => {
-    requestFullscreen(true);
+    requestFullscreen(false);
   };
 
   const handleWalletConnect = (data) => {
     const unityInstance = window.UnityLoader.getInstance();
-    unityInstance.SendMessage(
-      "MyGameObject",
-      "WalletConnected",
-      true
-    );
+    unityInstance.SendMessage('WalletConnectivity', 'WalletConnected', true);
   };
-
-
 
   return (
     <div className={styles.backgroundContainer}>
       <h1 className={styles.homePageTitle}> Abyss Seekers </h1>
       <div className={styles.gameContainer}>
-        {" "}
+        {' '}
         <Unity
           unityProvider={unityProvider}
-          style={{ width: 800, height: 600 }}
+          style={{ width: 1280, height: 720 }}
         />
       </div>
       <div className={styles.buttonMenu}>
         <MainButton onClick={requestFullScr}>Fullscreen</MainButton>
         <ConnectButton onConnectSuccess={handleWalletConnect}>
-          Connect your wallet{" "}
+          Connect your wallet{' '}
         </ConnectButton>
       </div>
     </div>

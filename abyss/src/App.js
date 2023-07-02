c80697d99c524f1b493775dc58811784d59e9b1e
tjuntji <tjuntjioon@gmail.com>
2023-07-02 18:48:16 +0800

    Reenabled all features except game

--- abyss/src/App.js
+++ abyss/src/App.js
@@ -1,50 +1,50 @@
 import React, { useEffect } from 'react';
-// import { Unity, useUnityContext } from 'react-unity-webgl';
-// import HomePage from './Components/HomePage';
-// import styles from './Components/ComponentCss/HomePage.module.css';
-// import MainButton from './Components/UI/MainButton';
-// import { ConnectButton } from '@suiet/wallet-kit';
-// import './overrideSuiButton.css';
+import { Unity, useUnityContext } from 'react-unity-webgl';
+import HomePage from './Components/HomePage';
+import styles from './Components/ComponentCss/HomePage.module.css';
+import MainButton from './Components/UI/MainButton';
+import { ConnectButton } from '@suiet/wallet-kit';
+import './overrideSuiButton.css';
 
 function App() {
-  // const { unityProvider, requestFullscreen, sendMessage } = useUnityContext({
-  //   loaderUrl: '/Build/Gamev02.loader.js',
-  //   dataUrl: '/Build/Gamev02.data',
-  //   frameworkUrl: '/Build/Gamev02.framework.js',
-  //   codeUrl: '/Build/Gamev02.wasm',
-  // });
+  const { unityProvider, requestFullscreen, sendMessage } = useUnityContext({
+    loaderUrl: '/Build/Gamev02.loader.js',
+    dataUrl: '/Build/Gamev02.data',
+    frameworkUrl: '/Build/Gamev02.framework.js',
+    codeUrl: '/Build/Gamev02.wasm',
+  });
 
-  // const requestFullScr = () => {
-  //   requestFullscreen(true);
-  // };
+  const requestFullScr = () => {
+    requestFullscreen(true);
+  };
 
-  // const handleWalletConnect = (data) => {
-  //   sendMessage('WalletConnectivity', 'WalletConnected');
-  // };
+  const handleWalletConnect = (data) => {
+    sendMessage('WalletConnectivity', 'WalletConnected');
+  };
 
-  // const handleWalletDisconnect = (data) => {
-  //   sendMessage('WalletConnectivity', 'WalletDisconnected');
-  // };
+  const handleWalletDisconnect = (data) => {
+    sendMessage('WalletConnectivity', 'WalletDisconnected');
+  };
 
   return (
     <div>
       <h1 > Aura Tale </h1>
-      {/* <div className={styles.gameContainer}>
-        {' '}
+      <div className={styles.gameContainer}>
+        {/* {' '}
         <Unity
           unityProvider={unityProvider}
           style={{ width: 1280, height: 720 }}
-        />
+        /> */}
       </div>
       <div className={styles.buttonMenu}>
-        <MainButton onClick={requestFullScr}>Fullscreen</MainButton>
+        {/* <MainButton onClick={requestFullScr}>Fullscreen</MainButton> */}
         <ConnectButton
           onConnectSuccess={handleWalletConnect}
           onDisconnectSuccess={handleWalletDisconnect}
         >
           Connect your wallet{' '}
         </ConnectButton>
-      </div> */}
+      </div>
     </div>
   );
 }

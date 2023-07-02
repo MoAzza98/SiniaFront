b5dddc368e421055c51a9ac89b48ca49dd31fb6d
tjuntji <tjuntjioon@gmail.com>
2023-07-02 18:41:57 +0800

    removed everything for debug

--- abyss/src/index.js
+++ abyss/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { WalletProvider } from '@suiet/wallet-kit';
-import '@suiet/wallet-kit/style.css';
+// import { WalletProvider } from '@suiet/wallet-kit';
+// import '@suiet/wallet-kit/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
